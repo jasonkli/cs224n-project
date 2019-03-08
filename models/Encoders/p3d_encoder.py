d399598c73489ad449d0df38c282d7aeb0c87d3f
Jason Li <jasonkli@stanford.edu>
2019-03-07 22:21:53 -0800

    Merge..

--- models/Encoders/p3d_encoder.py
+++ models/Encoders/p3d_encoder.py
@@ -11,10 +11,11 @@ device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 class P3DEncoder(nn.Module):
 
-	def __init__(self, feature_size=2048, attention_size=1024, hidden_decoder_size=512, dropout_rate=0.0):
+	def __init__(self, feature_size=2048, attention_size=None, hidden_decoder_size=512, dropout_rate=0.0):
 		
 		super().__init__()
-		self.feature_transform = nn.Linear(feature_size, attention_size)
+		if attention_size is not None:
+			self.feature_transform = nn.Linear(feature_size, attention_size)
 		self.hidden_transform = nn.Linear(feature_size, hidden_decoder_size)
 		self.cell_transform = nn.Linear(feature_size, hidden_decoder_size)
 		self.dropout = nn.Dropout(dropout_rate)
@@ -24,7 +25,10 @@ class P3DEncoder(nn.Module):
 		sum_features = torch.sum(features_padded, dim=0)
 		norm = torch.unsqueeze(torch.tensor(features_actual_lengths, device=device, dtype=torch.float), dim=1)
 		mean_features = sum_features / norm
-		mapped_features = self.feature_transform(features_padded)
+		if attention_size is not None:
+			mapped_features = self.feature_transform(features_padded)
+		else:
+			mapped_features = features_padded
 		hidden_layer1 = self.dropout(self.hidden_transform(mean_features))
 		hidden_layer2 = self.dropout(self.hidden_transform(mean_features))
 		cell_layer1 = self.dropout(self.cell_transform(mean_features))
